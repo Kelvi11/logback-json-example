cecbc428b3024993c9d0c388378796913356777c
dervishllari <kelvin.dervishllari@moveoalbania.com>
2022-10-31 14:47:38 +0100

    init

---
+++ src/main/java/com/example/logbackjsonexample/LogbackJsonExampleApplication.java
@@ -0,0 +1,15 @@
+package com.example.logbackjsonexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class LogbackJsonExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogbackJsonExampleApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/example/logbackjsonexample/LogbackJsonExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.logbackjsonexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogbackJsonExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
